06080dee89c427265d29d7691e944ae5953824dd
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-02 21:40:44 +0530

    update otpModel

--- src/models/OTP.ts
+++ src/models/OTP.ts
@@ -4,12 +4,15 @@ export interface IOtp extends Document{
     email: string
     otp: string
     expiresAt: Date
+    isValid : boolean
+    
 }
 
 const otpSchema = new mongoose.Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
+  expiresAt: { type: Date, required: true },
+  isValid: { type: Boolean, default: true}
 });
 
 export const OtpModel = mongoose.model("OTP", otpSchema);
