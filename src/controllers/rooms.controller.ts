cb4c028cc28160b56c9920cc8b170403238b733b
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-04 16:50:11 +0530

    create get all and delete

--- src/controllers/rooms.controller.ts
+++ src/controllers/rooms.controller.ts
@@ -1,14 +1,17 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { AuthRequest } from "../middleware/auth";
 import cloudinary from "../config/cloudinary";
 import { RoomModel } from "../models/Room";
 
 export const saveRoom = async (req: AuthRequest, res: Response) =>{
+    
     try {
-        if(req.user) {
-            return res.status(401).json({message:"Unauthorized"})
+        
+        if(!req.user) {
+            return res.status(401).json({isSave: false,message:"Unauthorized"})
         }
-
+        
+        console.log("method run")
         const{type, price, status, pax, bedType, amenities, count} = req.body
         let imageURL = ''
 
@@ -18,7 +21,7 @@ export const saveRoom = async (req: AuthRequest, res: Response) =>{
                     {folder: "imageRoom"},
                     (error, result) => {
                         if(error){
-                            console.error(error)
+                            console.error("image Not save",error)
                             return reject(error)
                         }
                         resole(result)
@@ -27,6 +30,11 @@ export const saveRoom = async (req: AuthRequest, res: Response) =>{
                 upload_stream.end(req.file?.buffer)
             })
             imageURL = result.secure_url
+            console.log(imageURL)
+        }
+
+        if(imageURL === ''){
+            return res.status(401).json({isSave: false, message:"image save fail"})
         }
 
         const newRoom = new RoomModel({
@@ -41,14 +49,42 @@ export const saveRoom = async (req: AuthRequest, res: Response) =>{
         })
         try {
             await newRoom.save()
+            
+            console.log("image save")
             res.status(201).json({
-                 message: "Save Room",
+                isSave: true,
+                message: "Save Room",
                 data: newRoom
             })
         } catch (error) {
-            res.status(500).json({message:`${error}`})
+            res.status(500).json({isSave: false, message:`${error}`})
         }
     } catch (error) {
-        res.status(500).json({message:`${error}`})
+        res.status(500).json({isSave: false,message:`${error}`})
     }
 }
+
+export const getAllRooms = async (req:Request, res:Response) => {
+    try {
+        const room =await RoomModel.find()
+
+        res.status(201).json({
+            message:"post data",
+            data: room
+        })
+    } catch (error) {
+        
+    }
+}
+
+export const deleteRoom = async (req:AuthRequest, res: Response) =>{
+    console.log("method run")
+    try {
+         const { id } = req.body
+         console.log(id)
+        await RoomModel.deleteOne({_id:id})
+        res.status(200).json({isDelete: true, message:"delete successfully"})
+    } catch (error) {
+        res.status(500).json({isDelete: true,message:"can't delete"})
+    }
+} 
