82b6e6d9baa6ad187be4dc1ce8e327ffd5191a58
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-05 10:56:28 +0530

    update profile save

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -8,29 +8,31 @@ export const saveProfile = async (req:AuthRequest, res: Response)=>{
         if(!req.user.sub){
             return res.status(400).json({message:"Unauthorized"})
         }
-
-        const exitProfile = await ProfileModel.findById(req.user.sub)
+console.log("methord 1")
+        const exitProfile = await ProfileModel.findOne({user:req.user.sub})
         if(exitProfile){
             return res.status(400).json({message:"User already"})
         }
-
+console.log("methord 2")
         const {address, phone, country} = req.body
 
         if(!address || !phone || !country){
             return res.status(400).json({message:"All are require"})
         }
-        console.log("run")
+console.log("methord 3")        
         const profile = new ProfileModel({
             user:req.user.sub,
+            image:'',
             address,
             phone,
             country
         })
 
-        await profile.save
+        await profile.save()
 
         res.status(200).json({isSave: true,message:"profile save"})
     } catch (error) {
+        console.log(error)
         return res.status(500).json({isSave: false,message:"profile save fail"})
     }
 }
--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,19 +1,20 @@
-import mongoose,{Document, Schema} from "mongoose"
+import mongoose, { Document, Schema } from "mongoose";
 
 export interface IProfile extends Document {
-    
-    user: mongoose.Types.ObjectId;
+    user: mongoose.Types.ObjectId
     image:string
     address: string
-    phone: number
+    phone: string
     country: string
 }
-const userSchema = new Schema<IProfile>({
+const profile: Schema = new Schema<IProfile>({
+    
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     image: { type: String },
     address: {type: String, required: true},
-    phone: {type: Number, required: true},
+    phone: {type: String, required: true},
     country: {type: String, required: true},
     
-})
-export const ProfileModel = mongoose.model<IProfile>("Profile", userSchema)
+    }, {timestamps: true,}
+)
+export const ProfileModel = mongoose.model<IProfile>("Profile", profile)
