c6e8fc7f8a8b4c6c377f7aa6898a980b61a05aac
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-06 18:16:50 +0530

    update origin

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://rainforest-royal-heritage-cabin-fro-gules.vercel.app/"],
+        origin: ["http://localhost:5173","https://rainforest-royal-heritage-cabin-fro-gules.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"]
     })
 )
